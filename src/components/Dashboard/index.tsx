32dbd3545241b10efde726fac962db4076054e00
Matheus Silva <matheus.silva@esx.com.br>
2022-03-16 22:36:39 -0300

    Criando component Summary

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Sumarry } from "../Summary";
+import { Container } from "./style";
+
+export function Dashboard () {
+    return (
+        <Container>
+            <Sumarry />
+        </Container>
+    );
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     :root {
         --bakground: #f0f2f5;
         --red: #E52E4D;
+        --green: #33CC95;
         --blue: #5429cc;
 
         --blue-light: #6933ff;
